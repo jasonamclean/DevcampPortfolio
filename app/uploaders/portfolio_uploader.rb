5d90e0ef25b2f523601444ee112a38f3c33ce4e9
Jason McLean <jasonamclean@hotmail.com>
2017-02-28 09:52:24 -0700

    Implement initial file uploading system via carrierwave for portfolio items

---
+++ app/uploaders/portfolio_uploader.rb
@@ -0,0 +1,16 @@
+class PortfolioUploader < CarrierWave::Uploader::Base
+
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+end
