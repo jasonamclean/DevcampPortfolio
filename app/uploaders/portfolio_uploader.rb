392d30173b864d3375988f841262730a43076205
Jason McLean <jasonamclean@hotmail.com>
2017-03-01 12:08:50 -0700

    Bug after finishing carrierwave installation

--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -1,14 +1,11 @@
 class PortfolioUploader < CarrierWave::Uploader::Base
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :aws
   
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
