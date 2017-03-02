b6ba551d674304231f60c8dc749e60b967babc64
Jason McLean <jasonamclean@hotmail.com>
2017-03-02 13:30:52 -0700

    Finish and fix carrierwave install with correct aws region code

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,8 +3,8 @@ class Portfolio < ApplicationRecord
 	accepts_nested_attributes_for :technologies,
 																reject_if: lambda { |attrs| attrs['name'].blank? }
 
-	include Placeholder
-	validates_presence_of :title, :body, :main_image, :thumb_image
+	# include Placeholder
+	validates_presence_of :title, :body
 
 	mount_uploader :thumb_image, PortfolioUploader
 	mount_uploader :main_image, PortfolioUploader
